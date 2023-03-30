c081e80fc36eab98982d62aa828df5b30713809b
Nicholas Orndorff <norndorff@live.com>
2023-03-30 10:49:48 -0700

    remove beamdef

--- asbeam/beamresidual.py
+++ asbeam/beamresidual.py
@@ -1,8 +1,6 @@
 import csdl
 import python_csdl_backend
 import numpy as np
-from transform import CalcNodalT
-from beamdef import BeamDef
 
 
 
@@ -49,28 +47,86 @@ class BeamRes(csdl.Model):
         # OMEGA = self.declare_variable('OMEGA',shape=(3)) # aircraft rotation
         # THETA = self.declare_variable('THETA',shape=(3)) # aircraft orientation Euler angles
 
-        self.add(BeamDef(options=options), name=name+'BeamDef')
-        self.add(CalcNodalT(options=options), name=name+'CalcNodalT')
-
         # other beam paramaters
         E_inv = self.declare_variable(name+'E_inv',shape=(3,3,n))
-        K_0 = self.declare_variable(name+'K_0',shape=(3,3,n))
         D = self.declare_variable(name+'D',shape=(3,3,n))
         oneover = self.declare_variable(name+'oneover',shape=(3,3,n))
         f = self.declare_variable(name+'f',shape=(3,n),val=0) # distributed forces
         m = self.declare_variable(name+'m',shape=(3,n),val=0) # distributed moments
         theta_0 = self.declare_variable(name+'theta_0',shape=(3,n),val=0)
         r_0 = self.declare_variable(name+'r_0',shape=(3,n),val=0)
-        T = self.declare_variable(name+'T',shape=(3,3,n)) # transformation tensor from xyz to csn
-        #K = self.declare_variable(name+'K',shape=(3,3,n)) # curvature/angle-rate relation tensor
         fp = self.declare_variable(name+'fp',shape=(3,n),val=0)
         mp = self.declare_variable(name+'mp',shape=(3,n),val=0)
         zero = self.declare_variable(name+'zero',val=0)
         one = self.declare_variable(name+'one',val=1)
 
 
-        # compute K: the curvature/angle-rate relation tensor
+        # compute T & T_0: the transformation tensor from xyz to csn
+        R_phi = self.create_output(name+'R_phi', shape=(n,3,3),val=0)
+        R_theta = self.create_output(name+'R_theta', shape=(n,3,3),val=0)
+        R_psi = self.create_output(name+'R_psi', shape=(n,3,3),val=0)
+        R_phi_0 = self.create_output(name+'R_phi_0', shape=(n,3,3),val=0)
+        R_theta_0 = self.create_output(name+'R_theta_0', shape=(n,3,3),val=0)
+        R_psi_0 = self.create_output(name+'R_psi_0', shape=(n,3,3),val=0)
+        T = self.create_output(name+'T',shape=(3,3,n),val=0)
+        T_0 = self.create_output(name+'T_0',shape=(3,3,n),val=0)
+        for i in range(0, n):
+            a1, a2, a3 = theta[0, i], theta[1, i], theta[2, i]
+            a1_0, a2_0, a3_0 = theta_0[0, i], theta_0[1, i], theta_0[2, i]
+            # rotation tensor for phi (angle a1)
+            R_phi[i,0,0] = csdl.expand(one,(1,1,1))
+            R_phi[i,1,1] = csdl.expand(csdl.cos(a1),(1,1,1),'ij->ijk')
+            R_phi[i,1,2] = csdl.expand(csdl.sin(a1),(1,1,1),'ij->ijk')
+            R_phi[i,2,1] = csdl.expand(-csdl.sin(a1),(1,1,1),'ij->ijk')
+            R_phi[i,2,2] = csdl.expand(csdl.cos(a1),(1,1,1),'ij->ijk')
+
+            R_phi_0[i,0,0] = csdl.expand(one,(1,1,1))
+            R_phi_0[i,1,1] = csdl.expand(csdl.cos(a1_0),(1,1,1),'ij->ijk')
+            R_phi_0[i,1,2] = csdl.expand(csdl.sin(a1_0),(1,1,1),'ij->ijk')
+            R_phi_0[i,2,1] = csdl.expand(-csdl.sin(a1_0),(1,1,1),'ij->ijk')
+            R_phi_0[i,2,2] = csdl.expand(csdl.cos(a1_0),(1,1,1),'ij->ijk')
+
+            # rotation tensor for theta (angle a2)
+            R_theta[i,0,0] = csdl.expand(csdl.cos(a2),(1,1,1),'ij->ijk')
+            R_theta[i,0,2] = csdl.expand(-csdl.sin(a2),(1,1,1),'ij->ijk')
+            R_theta[i,1,1] = csdl.expand(one,(1,1,1))
+            R_theta[i,2,0] = csdl.expand(csdl.sin(a2),(1,1,1),'ij->ijk')
+            R_theta[i,2,2] = csdl.expand(csdl.cos(a2),(1,1,1),'ij->ijk')
+
+            R_theta_0[i,0,0] = csdl.expand(csdl.cos(a2_0),(1,1,1),'ij->ijk')
+            R_theta_0[i,0,2] = csdl.expand(-csdl.sin(a2_0),(1,1,1),'ij->ijk')
+            R_theta_0[i,1,1] = csdl.expand(one,(1,1,1))
+            R_theta_0[i,2,0] = csdl.expand(csdl.sin(a2_0),(1,1,1),'ij->ijk')
+            R_theta_0[i,2,2] = csdl.expand(csdl.cos(a2_0),(1,1,1),'ij->ijk')
+
+            # rotation tensor for psi (angle a3)
+            R_psi[i,0,0] = csdl.expand(csdl.cos(a3),(1,1,1),'ij->ijk')
+            R_psi[i,0,1] = csdl.expand(csdl.sin(a3),(1,1,1),'ij->ijk')
+            R_psi[i,1,0] = csdl.expand(-csdl.sin(a3),(1,1,1),'ij->ijk')
+            R_psi[i,1,1] = csdl.expand(csdl.cos(a3),(1,1,1),'ij->ijk')
+            R_psi[i,2,2] = csdl.expand(one,(1,1,1))
+
+            R_psi_0[i,0,0] = csdl.expand(csdl.cos(a3_0),(1,1,1),'ij->ijk')
+            R_psi_0[i,0,1] = csdl.expand(csdl.sin(a3_0),(1,1,1),'ij->ijk')
+            R_psi_0[i,1,0] = csdl.expand(-csdl.sin(a3_0),(1,1,1),'ij->ijk')
+            R_psi_0[i,1,1] = csdl.expand(csdl.cos(a3_0),(1,1,1),'ij->ijk')
+            R_psi_0[i,2,2] = csdl.expand(one,(1,1,1))
+
+            if beam_type == 'wing':
+                T[:,:,i] = csdl.expand(csdl.matmat(csdl.reshape(R_theta[i,:,:], new_shape=(3,3)), csdl.matmat(csdl.reshape(R_psi[i,:,:], new_shape=(3,3)), csdl.reshape(R_phi[i,:,:], new_shape=(3,3)))), (3,3,1),'ij->ijk')
+                T_0[:,:,i] = csdl.expand(csdl.matmat(csdl.reshape(R_theta_0[i,:,:], new_shape=(3,3)), csdl.matmat(csdl.reshape(R_psi_0[i,:,:], new_shape=(3,3)), csdl.reshape(R_phi_0[i,:,:], new_shape=(3,3)))), (3,3,1),'ij->ijk')
+            elif beam_type == 'fuse':
+                T[:,:,i] = csdl.expand(csdl.matmat(csdl.reshape(R_theta[i,:,:], new_shape=(3,3)), csdl.matmat(csdl.reshape(R_phi[i,:,:], new_shape=(3,3)), csdl.reshape(R_psi[i,:,:], new_shape=(3,3)))), (3,3,1),'ij->ijk')
+                T_0[:,:,i] = csdl.expand(csdl.matmat(csdl.reshape(R_theta_0[i,:,:], new_shape=(3,3)), csdl.matmat(csdl.reshape(R_phi_0[i,:,:], new_shape=(3,3)), csdl.reshape(R_psi_0[i,:,:], new_shape=(3,3)))), (3,3,1),'ij->ijk')
+
+
+
+
+
+
+        # compute K & K_0: the curvature/angle-rate relation tensor
         K = self.create_output(name+'K',shape=(3,3,n),val=0)
+        K_0 = self.create_output(name+'K_0',shape=(3,3,n),val=0)
         for i in range(0, n):
             if beam_type == 'wing':
                 K[0,0,i] = csdl.expand(csdl.cos(theta[2,i])*csdl.cos(theta[1,i]),(1,1,1),'ij->ijk')
@@ -79,6 +135,14 @@ class BeamRes(csdl.Model):
                 K[1,1,i] = csdl.expand(one,(1,1,1))
                 K[2,0,i] = csdl.expand(csdl.cos(theta[2,i])*csdl.sin(theta[1,i]),(1,1,1),'ij->ijk')
                 K[2,2,i] = csdl.expand(csdl.cos(theta[1,i]),(1,1,1),'ij->ijk')
+
+                K_0[0,0,i] = csdl.reshape(csdl.cos(theta_0[2,i])*csdl.cos(theta_0[1,i]), new_shape=(1,1,1))
+                K_0[0,2,i] = csdl.reshape(-csdl.sin(theta_0[1,i]), new_shape=(1,1,1))
+                K_0[1,0,i] = csdl.reshape(-csdl.sin(theta_0[2,i]), new_shape=(1,1,1))
+                K_0[1,1,i] = csdl.reshape(one, new_shape=(1,1,1))
+                K_0[2,0,i] = csdl.reshape(csdl.cos(theta_0[2,1])*csdl.sin(theta_0[1,i]), new_shape=(1,1,1))
+                K_0[2,2,i] = csdl.reshape(csdl.cos(theta_0[1,i]), new_shape=(1,1,1))
+
             if beam_type == 'fuse':
                 K[0,0,i] = csdl.expand(csdl.cos(theta[1,i]),(1,1,1),'ij->ijk')
                 K[0,2,i] = csdl.expand(-csdl.cos(theta[0,i])*csdl.sin(theta[1,i]),(1,1,1),'ij->ijk')
@@ -86,6 +150,13 @@ class BeamRes(csdl.Model):
                 K[1,2,i] = csdl.expand(csdl.sin(theta[0,i]),(1,1,1),'ij->ijk')
                 K[2,0,i] = csdl.expand(csdl.sin(theta[1,i]),(1,1,1),'ij->ijk')
                 K[2,2,i] = csdl.expand(csdl.cos(theta[0,i])*csdl.cos(theta[1,i]),(1,1,1),'ij->ijk')
+
+                K_0[0,0,i] = csdl.reshape(csdl.cos(theta_0[1,i]),new_shape=(1,1,1))
+                K_0[0,2,i] = csdl.reshape(-csdl.cos(theta_0[0,i])*csdl.sin(theta_0[1,i]),new_shape=(1,1,1))
+                K_0[1,1,i] = csdl.reshape(one,new_shape=(1,1,1))
+                K_0[1,2,i] = csdl.reshape(csdl.sin(theta_0[0,i]),new_shape=(1,1,1))
+                K_0[2,0,i] = csdl.reshape(csdl.sin(theta_0[1,i]),new_shape=(1,1,1))
+                K_0[2,2,i] = csdl.reshape(csdl.cos(theta_0[0,i])*csdl.cos(theta_0[1,i]),new_shape=(1,1,1))
 
 
 
