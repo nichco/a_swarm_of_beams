d8044b80508368030654f2d258b9945c8b8f7f06
nichco <norndorff@live.com>
2023-04-03 15:59:12 -0700

    testing

--- asbeam/aero.py
+++ asbeam/aero.py
@@ -11,11 +11,9 @@ class AeroSolver(csdl.Model):
         self.parameters.declare('n')
     def define(self):
         n = self.parameters['n']
-
-        B = self.declare_variable('B',shape=(n-2,n-2))
-        LHS = self.declare_variable('LHS',shape=(n-2))
-        A = self.declare_variable('A',shape=(n-2))
-
+        B = self.declare_variable('B',shape=(n,n))
+        LHS = self.declare_variable('LHS',shape=(n))
+        A = self.declare_variable('A',shape=(n))
         residual = csdl.matvec(B,A) - LHS
         self.register_output('residual', residual)
 
@@ -26,58 +24,40 @@ class Aero(csdl.Model):
     def define(self):
         N = self.parameters['N']
 
-
-        y = self.declare_variable('y',shape=(N),val=0)
-        tr = self.declare_variable('tr',val=0.6)
         croot = self.declare_variable('croot',val=2.1)
-        alpha = self.declare_variable('alpha',shape=(N),val=2)
-        alpha_0 = self.declare_variable('zero_lift_aoa',val=-1.5)
-        cla = self.declare_variable('cla',val=6.3)
+        ctip = self.declare_variable('ctip',val=1)
+        tr = ctip/croot
         b = self.declare_variable('b',val=10)
+        cla = self.declare_variable('cla',val=6.3) # (rad^-1)
+        alpha_0 = self.declare_variable('zero_lift_aoa',val=-1.5) # (deg)
+        alpha = self.declare_variable('alpha',shape=(N),val=2)
 
-        theta = self.create_output('theta',shape=(N),val=0)
-        for i in range(N):
-            theta[i] = csdl.arccos(2*((y[i]**2)**0.5)/b)
-        #self.print_var(theta)
+        S = b*((ctip+croot)/2)
+        AR = (b**2)/S
+
+        theta = self.create_input('theta',val=np.linspace(np.pi/(2*N),np.pi/2,N),shape=(N)) # angular position of each segment (rad)
+
+        #theta = self.create_output('theta',shape=(N),val=0)
+        #for i in range(N):
+        #    theta[i] = csdl.arccos(2*((y[i]**2)**0.5)/b)
         
         c = self.create_output('c',shape=(N),val=0)
-        for i in range(N):
-            c[i] = croot*(1+(tr-1)*csdl.cos(theta[i]))
-        #self.print_var(c)
+        for i in range(N): c[i] = croot*(1+(tr-1)*csdl.cos(theta[i]))
         
         mu = c*csdl.expand(cla/(4*b), (N))
-        self.print_var(mu)
         
         LHS = mu*(alpha - csdl.expand(alpha_0, (N)))/57.3
         self.register_output('LHS',LHS)
-
-        self.print_var(LHS)
-
-        
+     
         B = self.create_output('B',shape=(N,N))
-        #for i in range(1,n-1):
-        #    for j in range(1,n-1):
-        #        term1 = csdl.sin((2*(j) - 1)*theta[i-1])
-        #        term2 = 1 + (mu[i-1]*(2*(j) - 1))/csdl.sin(theta[i-1])
-        #        B[i-1,j-1] = csdl.expand(term1*term2, (1,1))
-        
-
-
         for i in range(0,N):
             for j in range(0,N):
                 term1 = csdl.sin((2*(j+1) - 1)*theta[i])
                 term2 = (1 + (mu[i]*(2*(j+1)-1)))/csdl.sin(theta[i])
                 B[i,j] = csdl.expand(term1*term2, (1,1), 'i->ij')
-        
-
-        #for i=1:N
-        #    for j=1:N
-        #        B(i,j) = sin((2*j-1) * theta(i)) * (1 + (mu(i) * (2*j-1)) / sin(theta(i)));
-        #    end
-        #end
 
 
-        solver = self.create_implicit_operation(AeroSolver(n=n))
+        solver = self.create_implicit_operation(AeroSolver(n=N))
         solver.declare_state('A', residual='residual')
         solver.nonlinear_solver = csdl.NewtonSolver(
         solve_subsystems=False,
@@ -86,14 +66,21 @@ class Aero(csdl.Model):
         solver.linear_solver = csdl.ScipyKrylov()
 
         A = solver(B,LHS)
-        self.print_var(A)
-        #self.print_var(LHS)
-
-        s = b*((croot*(tr+1))/2)
-        AR = b**2/s
 
         cl = np.pi*AR*A[0]
+        self.register_output('cl',cl)
         self.print_var(cl)
+
+        delta_vec = self.create_output('delta',shape=(N),val=0)
+        for i in range(2,N):
+            delta_vec[i] = (2*i - 1)*((A[i-1]/A[0])**2)
+
+        delta = csdl.sum(delta_vec)
+        e = 1/(1+delta)
+        cdi = (cl**2)/(np.pi*AR*e)
+
+        self.print_var(cdi)
+        self.register_output('cdi',cdi)
         
 
 
@@ -101,18 +88,6 @@ class Aero(csdl.Model):
 
 if __name__ == '__main__':
 
-    n = 21 # num nodes
-    N = n - 2
-    b = 14 # span
-    y = np.zeros((n))
-    y_in = y[1:-1]
-    for i in range(n):
-        y[i] = (-b/2)*(1-((2*i)/(n-1)))
 
-    #print(y_in)
-
-
-    sim = python_csdl_backend.Simulator(Aero(N=N))
-    sim['y'] = y_in
-    sim['b'] = b
+    sim = python_csdl_backend.Simulator(Aero(N=9))
     sim.run()
